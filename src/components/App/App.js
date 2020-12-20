26147c6b49358c62f7ce8aeee3bd2e9a8bda5e85
Audry Wolters <audrywolters@gmail.com>
2020-12-20 14:10:31 -0600

    restructering

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Catalog from '../Catalog/Catalog';
+
+function App() {
+  return (
+    <>
+      <Catalog />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Catalog/Catalog.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Catalog extends Component {
+
+    render() {
+        return (
+            <>
+                <div>hieeeeee</div>
+            </>
+        )
+    }
+}
+
+export default Catalog;
