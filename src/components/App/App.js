c8db7f3a1dff033f1017b9770e1712c8cb79076d
Audry Wolters <audrywolters@gmail.com>
2020-12-23 20:52:11 -0600

    moving along. errors as expected.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,56 @@
-import React from "react";
-import Main from "../Main/Main";
+import React, { Component } from "react";
+import Catalog from "../Catalog/Catalog";
+import Queue from "../Queue/Queue";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <Main />
-      {/* cool meta stuff to come */}
-    </>
-  );
+class App extends Component {
+  state = {
+    movieList: [],
+  };
+
+  componentDidMount() {
+    this.getMovieList();
+  }
+
+  getMovieList() {
+    let mockDBdata = 
+      [
+      { id: 1, title: "one",   poster: "images/1.jpg", isInQueue: true, queuePosition: 1 },
+      { id: 2, title: "two",   poster: "images/2.jpg", isInQueue: true, queuePosition: 2 },
+      { id: 3, title: "three", poster: "images/3.jpg", isInQueue: false, queuePosition: 3 },
+      { id: 4, title: "four",  poster: "images/4.jpg", isInQueue: false, queuePosition: 4 },
+      { id: 5, title: "five",  poster: "images/5.jpg", isInQueue: false, queuePosition: 5 },
+      { id: 6, title: "six",   poster: "images/6.jpg", isInQueue: false, queuePosition: 6 },
+      { id: 7, title: "seven", poster: "images/7.jpg", isInQueue: false, queuePosition: 7 }
+    ];
+
+    this.setState({
+      movieList: mockDBdata,
+    });
+  }
+
+  render() {
+
+    // movies to browse and pick
+    let catalogMovieList = this.state.movieList.filter(
+      movie => movie.isInQueue === false
+    );
+
+    // movies that user is going to watch
+    let queueMovieList = this.state.movieList.filter(
+      movie => movie.isInQueue === true
+    );
+
+    return (
+      <>
+        <header>AudryFlix</header>
+        <section>
+          <Catalog catalogMovieList={catalogMovieList} />
+          <Queue queueMovieList={queueMovieList} />
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import movieList from "./movieListReducer";
-
-const rootReducer = combineReducers({
-  movieList
-});
-
-export default rootReducer;
