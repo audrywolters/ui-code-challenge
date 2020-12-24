c46d5efcac8d23d22503b3baf628c2cc15f85062
Audry Wolters <audrywolters@gmail.com>
2020-12-23 23:55:14 -0600

    button changes + or - depending on which list

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,36 +1,24 @@
 import React, { Component } from "react";
-import './Movie.css';
+import "./Movie.css";
 
 class Movie extends Component {
-
   // state = {
-    
+
   // };
 
-  
   onClickAddOrRemove = () => {
     //console.log('click from movie ', this.props.id);
     this.props.onClickAddOrRemove(this.props.id);
-  }
+  };
 
   render() {
     return (
       <div className="movieContainer">
         <span>{this.props.title}</span>
-        <img alt="the movie poster" src={this.props.poster} />
-
-        {/* we need to if statement here too ( - ) */}
+        <img alt="Poster image of movie" src={this.props.poster} />
         <button id={this.props.id} onClick={this.onClickAddOrRemove}>
-          +
+          {this.props.isInQueue ? "-" : "+"}
         </button>
-        {/* 
-        {
-            this.state.isDisplayingImage
-            ? 
-            <img src={ this.props.thisPicture.path } />
-            :
-            <div className="description">{ this.props.thisPicture.description }</div>    
-          } */}
       </div>
     );
   }
